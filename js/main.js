16a3ebe9219d9d3da753a60e385f55664348c05b
Sultan <87053811230s@gmail.com>
2022-06-24 16:51:37 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let storeName = "Azalia";
+
+const storeDescription = {
+    budget: 10000,
+    employees: ['Zhemis', 'Asel', 'Tolkyn'],
+    products: {
+        alpenGold: 370,
+        nestle: 400
+    },
+    open: true
+};
+
+console.log(storeDescription)
